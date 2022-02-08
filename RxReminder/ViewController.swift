0071bd2db087aeb7364827c741677a0d506a3d8a
sanjidanisha <snisha000@citymail.cuny.edu>
2022-02-08 01:31:54 -0500

    Initial Commit

---
+++ RxReminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxReminder
+//
+//  Created by Sanjida Ayman Nisha on 2/8/22.
+//  Copyright © 2022 Sanjida Nisha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RxReminderTests/RxReminderTests.swift
@@ -0,0 +1,34 @@
+//
+//  RxReminderTests.swift
+//  RxReminderTests
+//
+//  Created by Sanjida Ayman Nisha on 2/8/22.
+//  Copyright © 2022 Sanjida Nisha. All rights reserved.
+//
+
+import XCTest
+@testable import RxReminder
+
+class RxReminderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
