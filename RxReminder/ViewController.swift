a252f5a7111a224bf93e80447bd28634489c119a
Nour Elabbasy <nour102600@gmail.com>
2022-02-10 00:54:59 -0500

    initial UI and some backend work

--- RxReminder/ViewController.swift
+++ RxReminder/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseAuthUI
 
 class ViewController: UIViewController {
     var ref: DatabaseReference!
@@ -21,11 +22,64 @@ class ViewController: UIViewController {
         
         self.ref.child("user_id").setValue(123456)
         self.ref.child("name").setValue("Nisha")
-        self.ref.child("age").setValue(56)
+        self.ref.child("age").setValue(65)
         
     }
     
+    @IBOutlet weak var usernameIcon: UITextField!{
+       didSet {
+          usernameIcon.tintColor = UIColor.lightGray
+          usernameIcon.setIcon(UIImage(named: "username")!)
+       }
+    }
+    
+    @IBOutlet weak var passwordIcon: UITextField!{
+       didSet {
+          passwordIcon.tintColor = UIColor.lightGray
+          passwordIcon.setIcon(UIImage(named: "key")!)
+       }
+    }
+    
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        guard authUI != nil else {
+            return
+        }
+        
+        authUI?.delegate = self
+        
+        let authViewController = authUI!.authViewController()
+        
+        present(authViewController, animated: true, completion: nil)
+        
+    }
 
+}
 
+extension UITextField {
+   func setIcon(_ image: UIImage) {
+   let iconView = UIImageView(frame:
+                  CGRect(x: 10, y: 5, width: 20, height: 20))
+   iconView.image = image
+   let iconContainerView: UIView = UIView(frame:
+                  CGRect(x: 20, y: 0, width: 30, height: 30))
+   iconContainerView.addSubview(iconView)
+   leftView = iconContainerView
+   leftViewMode = .always
+}
+}
+
+extension ViewController: FUIAuthDelegate {
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?)
+    {
+        if error != nil {
+            return
+        }
+        performSegue(withIdentifier: "HomePage", sender: self)
+    }
 }
 
