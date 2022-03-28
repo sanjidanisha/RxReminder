ff8b86b9ffd3878eb2f0e17c9f0364ed51078b8e
sanjidanisha <snisha000@citymail.cuny.edu>
2022-03-27 21:19:17 -0400

    added calendar page

---
+++ RxReminder/CalendarCell.swift
@@ -0,0 +1,19 @@
+//
+//  CalendarCell.swift
+//  RxReminder
+//
+//  Created by Sanjida Ayman Nisha on 3/26/22.
+//  Copyright © 2022 Sanjida Nisha. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+  
+   
+    @IBOutlet weak var dayOfMonth: UILabel!
+    
+}
+
---
+++ RxReminder/CalendarCellViewController.swift
@@ -0,0 +1,98 @@
+//
+//  CalendarCellViewController.swift
+//  RxReminder
+//
+//  Created by Sanjida Ayman Nisha on 3/26/22.
+//  Copyright © 2022 Sanjida Nisha. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCellViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    var selectedDate = Date()
+    var totalSquares = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setCellsView()
+        setMonthView()
+
+        // Do any additional setup after loading the view.
+    }
+    func setCellsView()
+    {
+        let width = (collectionView.frame.size.width - 2) / 8
+        let height = (collectionView.frame.size.height - 2) / 8
+        
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.itemSize = CGSize(width: width, height: height)
+    }
+    
+    func setMonthView()
+    {
+        totalSquares.removeAll()
+        
+        let daysInMonth = CalendarHelper().daysInMonth(date: selectedDate)
+        let firstDayOfMonth = CalendarHelper().firstOfMonth(date: selectedDate)
+        let startingSpaces = CalendarHelper().weekDay(date: firstDayOfMonth)
+        
+        var count: Int = 1
+        
+        while(count <= 42)
+        {
+            if(count <= startingSpaces || count - startingSpaces > daysInMonth)
+            {
+                totalSquares.append("")
+            }
+            else
+            {
+                totalSquares.append(String(count - startingSpaces))
+            }
+            count += 1
+        }
+        
+        monthLabel.text = CalendarHelper().monthString(date: selectedDate)
+            + " " + CalendarHelper().yearString(date: selectedDate)
+        collectionView.reloadData()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        totalSquares.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calCell", for: indexPath) as! CalendarCell
+        
+        cell.dayOfMonth.text = totalSquares[indexPath.item]
+        
+        return cell
+    }
+    
+   @IBAction func previousMonth(_ sender: Any) {
+          selectedDate = CalendarHelper().minusMonth(date: selectedDate)
+          setMonthView()
+      }
+      
+      
+      @IBAction func nextMonth(_ sender: Any) {
+          selectedDate = CalendarHelper().plusMonth(date: selectedDate)
+          setMonthView()
+      }
+    override open var shouldAutorotate: Bool
+    {
+        return false
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
